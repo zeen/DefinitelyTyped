08b94b81cd6fcadf99efb13a6e06924d98123e15
Waqas Hussain <waqas20@gmail.com>
2020-05-20 15:19:03 -0500

    [parquetjs] Add precise schema description types

--- types/parquetjs/lib/schema.interface.d.ts
+++ types/parquetjs/lib/schema.interface.d.ts
@@ -5,8 +5,9 @@ export interface SchemaInterface {
 }
 
 export interface SingleFieldInterface {
-    type: string;
-    encoding?: string;
+    type: 'BOOLEAN' | 'INT32' | 'INT64' | 'INT96' | 'FLOAT' | 'DOUBLE' | 'BYTE_ARRAY' | 'FIXED_LEN_BYTE_ARRAY' | 'UTF8' | 'TIME_MILLIS' | 'TIME_MICROS' | 'DATE' | 'TIMESTAMP_MILLIS' | 'TIMESTAMP_MICROS' | 'UINT_8' | 'UINT_16' | 'UINT_32' | 'UINT_64' | 'INT_8' | 'INT_16' | 'INT_32' | 'INT_64' | 'JSON' | 'BSON' | 'INTERVAL';
+    encoding?: 'PLAIN' | 'RLE';
+    compression?: 'UNCOMPRESSED' | 'GZIP' | 'SNAPPY' | 'LZO' | 'BROTLI';
     bitWidth?: number;
     optional?: boolean;
     repeated?: boolean;
